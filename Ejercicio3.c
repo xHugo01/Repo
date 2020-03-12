96b54a35377288494d775c25a06b4209dd89ffda
xHugo01 <57646309+xHugo01@users.noreply.github.com>
2020-03-12 17:23:26 +0100

    Create Ejercicio3.c

---
+++ Ejercicio3.c
@@ -0,0 +1,18 @@
+/**
+* Title: Ejercicio_3
+* Description: Paso de int a char
+* @author: H. de la Cámara
+* @organization: University of Burgos
+* @date: 2020/02/11
+* @version: 1.0
+*/
+
+#include<stdio.h>
+
+int main() {
+	int val;
+	printf("Introduce un numero: ");
+	scanf("%i",&val);
+	printf("El caracter es: %c\n",val);
+	return 0;
+}
